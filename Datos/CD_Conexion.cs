76efd2d7c16ea1a25b9e46f3afb3bdd9126696b1
Sergio <Sergio99_git@outlook.com>
2023-11-24 09:28:09 -0600

    Cambios del Miercoles

---
+++ Datos/CD_Conexion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Datos
+{
+    public class CD_Conexion
+    {
+        private string Bd;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        
+        private CD_Conexion()
+        {
+            this.Bd = "MiBaseDeDatos";
+            this.Servidor = "DESKTOP-7KHO1J8";
+            this.Usuario = "";
+            this.Clave = "";
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Conexion =  new SqlConnection();
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Conexion = null;
+                throw ex;
+            }
+            return Conexion;
+        }
+    }
+}
--- Presentacion/frm_principal.Designer.cs
+++ Presentacion/frm_principal.Designer.cs
@@ -31,10 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_principal));
             this.miMenu = new System.Windows.Forms.MenuStrip();
             this.datosMaestrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.procesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.sistemasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.salirDelSistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.miPrimerFormularioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.trackbarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.trabajandoConComboBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -42,12 +38,21 @@
             this.numericUpDownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.encuestaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.procesoTotalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.proceso1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.proceso2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.detalleDeProcesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sistemasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirDelSistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.m_btn_01 = new System.Windows.Forms.ToolStripButton();
+            this.m_btn_02 = new System.Windows.Forms.ToolStripButton();
+            this.m_btn_03 = new System.Windows.Forms.ToolStripButton();
             this.miMenu.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // miMenu
@@ -77,32 +82,6 @@
             this.datosMaestrosToolStripMenuItem.Name = "datosMaestrosToolStripMenuItem";
             this.datosMaestrosToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
             this.datosMaestrosToolStripMenuItem.Text = "Datos Maestros";
-            // 
-            // procesosToolStripMenuItem
-            // 
-            this.procesosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.procesoTotalToolStripMenuItem});
-            this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
-            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
-            this.procesosToolStripMenuItem.Text = "Procesos";
-            // 
-            // reportesToolStripMenuItem
-            // 
-            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
-            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
-            this.reportesToolStripMenuItem.Text = "Reportes";
-            // 
-            // sistemasToolStripMenuItem
-            // 
-            this.sistemasToolStripMenuItem.Name = "sistemasToolStripMenuItem";
-            this.sistemasToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
-            this.sistemasToolStripMenuItem.Text = "Sistemas";
-            // 
-            // salirDelSistemaToolStripMenuItem
-            // 
-            this.salirDelSistemaToolStripMenuItem.Name = "salirDelSistemaToolStripMenuItem";
-            this.salirDelSistemaToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
-            this.salirDelSistemaToolStripMenuItem.Text = "Salir del Sistema";
             // 
             // miPrimerFormularioToolStripMenuItem
             // 
@@ -149,6 +128,14 @@
             this.dataGridViewToolStripMenuItem.Size = new System.Drawing.Size(218, 22);
             this.dataGridViewToolStripMenuItem.Text = "DataGridView";
             // 
+            // procesosToolStripMenuItem
+            // 
+            this.procesosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.procesoTotalToolStripMenuItem});
+            this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
+            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.procesosToolStripMenuItem.Text = "Procesos";
+            // 
             // procesoTotalToolStripMenuItem
             // 
             this.procesoTotalToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -157,37 +144,105 @@
             this.toolStripSeparator1,
             this.detalleDeProcesosToolStripMenuItem});
             this.procesoTotalToolStripMenuItem.Name = "procesoTotalToolStripMenuItem";
-            this.procesoTotalToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.procesoTotalToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
             this.procesoTotalToolStripMenuItem.Text = "Proceso Total";
             // 
             // proceso1ToolStripMenuItem
             // 
             this.proceso1ToolStripMenuItem.Name = "proceso1ToolStripMenuItem";
-            this.proceso1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.proceso1ToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
             this.proceso1ToolStripMenuItem.Text = "Proceso 1";
             // 
             // proceso2ToolStripMenuItem
             // 
             this.proceso2ToolStripMenuItem.Name = "proceso2ToolStripMenuItem";
-            this.proceso2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.proceso2ToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
             this.proceso2ToolStripMenuItem.Text = "Proceso 2";
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(173, 6);
             // 
             // detalleDeProcesosToolStripMenuItem
             // 
             this.detalleDeProcesosToolStripMenuItem.Name = "detalleDeProcesosToolStripMenuItem";
-            this.detalleDeProcesosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.detalleDeProcesosToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
             this.detalleDeProcesosToolStripMenuItem.Text = "Detalle de Procesos";
+            // 
+            // reportesToolStripMenuItem
+            // 
+            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.reportesToolStripMenuItem.Text = "Reportes";
+            // 
+            // sistemasToolStripMenuItem
+            // 
+            this.sistemasToolStripMenuItem.Name = "sistemasToolStripMenuItem";
+            this.sistemasToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.sistemasToolStripMenuItem.Text = "Sistemas";
+            // 
+            // salirDelSistemaToolStripMenuItem
+            // 
+            this.salirDelSistemaToolStripMenuItem.Name = "salirDelSistemaToolStripMenuItem";
+            this.salirDelSistemaToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.salirDelSistemaToolStripMenuItem.Text = "Salir del Sistema";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.m_btn_01,
+            this.m_btn_02,
+            this.m_btn_03});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 53);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // m_btn_01
+            // 
+            this.m_btn_01.AutoSize = false;
+            this.m_btn_01.Image = ((System.Drawing.Image)(resources.GetObject("m_btn_01.Image")));
+            this.m_btn_01.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.m_btn_01.Name = "m_btn_01";
+            this.m_btn_01.Size = new System.Drawing.Size(80, 50);
+            this.m_btn_01.Text = "Mi 1er Form";
+            this.m_btn_01.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.m_btn_01.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.m_btn_01.ToolTipText = "Mi Primer Formulario";
+            this.m_btn_01.Click += new System.EventHandler(this.m_btn_01_Click);
+            // 
+            // m_btn_02
+            // 
+            this.m_btn_02.AutoSize = false;
+            this.m_btn_02.Image = ((System.Drawing.Image)(resources.GetObject("m_btn_02.Image")));
+            this.m_btn_02.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.m_btn_02.Name = "m_btn_02";
+            this.m_btn_02.Size = new System.Drawing.Size(80, 50);
+            this.m_btn_02.Text = "trackbar";
+            this.m_btn_02.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.m_btn_02.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.m_btn_02.ToolTipText = "Formulario trackbar";
+            // 
+            // m_btn_03
+            // 
+            this.m_btn_03.AutoSize = false;
+            this.m_btn_03.Image = ((System.Drawing.Image)(resources.GetObject("m_btn_03.Image")));
+            this.m_btn_03.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.m_btn_03.Name = "m_btn_03";
+            this.m_btn_03.Size = new System.Drawing.Size(80, 50);
+            this.m_btn_03.Text = "Elemento 3";
+            this.m_btn_03.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.m_btn_03.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.m_btn_03.ToolTipText = "Elemento vacio";
             // 
             // frm_principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.miMenu);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.miMenu;
@@ -195,6 +250,8 @@
             this.Text = "Contenedor Principal";
             this.miMenu.ResumeLayout(false);
             this.miMenu.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,5 +277,9 @@
         private System.Windows.Forms.ToolStripMenuItem proceso2ToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem detalleDeProcesosToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton m_btn_01;
+        private System.Windows.Forms.ToolStripButton m_btn_02;
+        private System.Windows.Forms.ToolStripButton m_btn_03;
     }
 }
